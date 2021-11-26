950f9ccc00a26cbef87d08152490c1e0b49149dd
Nakamol180 <nakamol_nuang@cmu.ac.th>
2021-11-26 16:45:45 +0700

    "lab6_1.cpp completed"

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
+    int N,even = 0,odd = 0 ;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> N; 
+
+    while (N!=0){
+        cout << "Enter an integer: ";
+        cin >> N;
+        
+        if( N%2 == 0 ){
+            even +=1;
+        }else{
+            odd+=1;
+        }
+    }
+    cout << "#Even numbers = " << even <<"\n";
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
